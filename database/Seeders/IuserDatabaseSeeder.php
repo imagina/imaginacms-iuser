644f380d600031a55abb8b0028770473c5aaa380
msolanogithub <msolano2505@gmail.com>
2025-08-28 12:59:14 -0500

    First commit v12.x

---
+++ database/Seeders/IuserDatabaseSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Modules\Iuser\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class IuserDatabaseSeeder extends Seeder
+{
+
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        $this->call(CreateRolesSeeder::class);
+    }
+}
