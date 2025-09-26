cc76a5a366047123ba3923f0c9fdc7b16ea4bcab
josuebedoya348 <josuebedoya348@gmail.com>
2025-09-26 09:52:51 -0500

    + Added url token to message

--- resources/lang/es/emails.php
+++ resources/lang/es/emails.php
@@ -7,7 +7,9 @@ return [
         'message' => "Hola,<br><br>
         Has solicitado un cambio de contraseña. Haz clic en el siguiente enlace para restablecer tu contraseña:<br><br>
         <a href=':url' target='_blank' style='color: #1a73e8; text-decoration: underline;'>Haz clic aquí</a><br><br>
-        Si no realizaste esta solicitud, puedes ignorar este mensaje.",
+        O puedes copiar y/o navegar el siguiente enlace<br>
+        <a href=':url' target='_blank' style='color: #1a73e8; text-decoration: underline;'>:url</a>
+        <br><br>Si no realizaste esta solicitud, puedes ignorar este mensaje. Su contraseña actual permanecerá intacta.",
     ],
 
 ];
